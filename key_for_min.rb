0405a88352c5d67b17508de50b013d5de1c2d4f5
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-02-21 00:29:03 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  if name_hash != {}
+    name_array = []
+    name_hash.each do |key, value|
+      name_array << "#{value},#{key}"
+    end
+  sorted_array = name_array.sort
+  lowest_pair = sorted_array[0].split(",")
+  lowest_pair[1].to_sym
+end
 end
